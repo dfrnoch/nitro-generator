e22be10fd5b9128131e498c76dde01b3b862b23c
LnX <in@lnx.rip>
2020-08-28 16:16:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from colorama import Fore
 import random
 import string
 from time import sleep
+import requests
 
 
 os.system("cls")
@@ -11,7 +12,7 @@ print(f"{Fore.WHITE}[ {Fore.CYAN}§ {Fore.WHITE}] {Fore.LIGHTBLACK_EX}Discord Ni
 print(f"{Fore.WHITE}[ {Fore.CYAN}§ {Fore.WHITE}] {Fore.LIGHTBLACK_EX}You can follow me on Github: {Fore.WHITE}https://github.com/lnxcz")
 amount = int(input(f"\n{Fore.WHITE}[ {Fore.YELLOW}> {Fore.WHITE}] {Fore.LIGHTBLACK_EX}How much codes will be generated: {Fore.WHITE}"))
 print(f"\n{Fore.WHITE}[ {Fore.YELLOW}? {Fore.WHITE}] {Fore.LIGHTBLACK_EX}Classic Nitro is 16chars and Boost Nitro is 24chars")
-nitro = input(f"{Fore.WHITE}[ {Fore.YELLOW}> {Fore.WHITE}] {Fore.LIGHTBLACK_EX}Boost codes or Classic codes (boost or classic): {Fore.WHITE}")
+nitro = input(f"{Fore.WHITE}[ {Fore.YELLOW}> {Fore.WHITE}] {Fore.LIGHTBLACK_EX}Boost codes or Classic codes {Fore.WHITE}(boost or classic){Fore.LIGHTBLACK_EX}: {Fore.WHITE}")
 
 if "boost" in nitro or "classic" in nitro:
     pass
@@ -19,33 +20,104 @@ else:
     print(f"{Fore.WHITE}[ {Fore.RED}! {Fore.WHITE}] {Fore.LIGHTBLACK_EX}Answer must be {Fore.WHITE}boost {Fore.LIGHTBLACK_EX}or {Fore.WHITE}classic")
     exit()
 
-print(f"\n{Fore.WHITE}[ {Fore.YELLOW}? {Fore.WHITE}] {Fore.LIGHTBLACK_EX}If true, before code will be {Fore.WHITE}discord.gift/")
-prefix = input(f"{Fore.WHITE}[ {Fore.YELLOW}> {Fore.WHITE}] {Fore.LIGHTBLACK_EX}Prefix before codes (yes or no): {Fore.WHITE}")
-if "yes" in prefix or "no" in prefix:
-    pass
-else:
-    print(f"{Fore.WHITE}[ {Fore.RED}! {Fore.WHITE}] {Fore.LIGHTBLACK_EX}Answer must be {Fore.WHITE}yes {Fore.LIGHTBLACK_EX}or {Fore.WHITE}no")
-    exit()
 
+checker = input(f"{Fore.WHITE}[ {Fore.YELLOW}> {Fore.WHITE}] {Fore.LIGHTBLACK_EX}Enable Checker {Fore.WHITE}(yes or no){Fore.LIGHTBLACK_EX}: {Fore.WHITE}")
+
+def scrape():
+    scraped = 0
+    f = open("proxies.txt", "a+")
+    f.truncate(0)
+    r = requests.get('https://api.proxyscrape.com/?request=displayproxies&proxytype=http&timeout=1500&ssl=yes')
+    proxies = []
+    for proxy in r.text.split('\n'):
+        proxy = proxy.strip()
+        if proxy:
+            proxies.append(proxy)
+    for p in proxies:
+        scraped = scraped + 1 
+        f.write((p)+"\n")
+    f.close()
+    print(f"{Fore.WHITE}[ {Fore.YELLOW}? {Fore.WHITE}] {Fore.LIGHTBLACK_EX}Scraped {Fore.WHITE}{scraped} {Fore.LIGHTBLACK_EX}proxies.")
+
+if checker == "yes":
+    scrape = input(f"{Fore.WHITE}[ {Fore.YELLOW}> {Fore.WHITE}] {Fore.LIGHTBLACK_EX}Auto proxy scrape {Fore.WHITE}(yes or no){Fore.LIGHTBLACK_EX}: {Fore.WHITE}")
+else:
+    print(f"\n{Fore.WHITE}[ {Fore.YELLOW}? {Fore.WHITE}] {Fore.LIGHTBLACK_EX}If true, before code will be {Fore.WHITE}discord.gift/")
+    prefix = input(f"{Fore.WHITE}[ {Fore.YELLOW}> {Fore.WHITE}] {Fore.LIGHTBLACK_EX}Prefix before codes {Fore.WHITE}(yes or no){Fore.LIGHTBLACK_EX}: {Fore.WHITE}")
+    if "yes" in prefix or "no" in prefix:
+        pass
+    else:
+        print(f"{Fore.WHITE}[ {Fore.RED}! {Fore.WHITE}] {Fore.LIGHTBLACK_EX}Answer must be {Fore.WHITE}yes {Fore.LIGHTBLACK_EX}or {Fore.WHITE}no")
+        exit()
+
+if scrape == "yes":
+    scrape()
 print(f"\n{Fore.WHITE}[ {Fore.YELLOW}? {Fore.WHITE}] {Fore.LIGHTBLACK_EX}Generating {Fore.WHITE}{amount}{Fore.LIGHTBLACK_EX} codes!")
-sleep(1)
+if checker != "yes":
+    sleep(1)
 
 fulla = amount
-
 f = open(f"codes.txt","w+", encoding="UTF-8")
+p = open(f"proxies.txt", encoding="UTF-8")
 
-while amount > 0:
-    amount = amount - 1
-    if "boost" in nitro:
-        code = ''.join([random.choice(string.ascii_letters + string.digits) for i in range(24)])
-    if "classic" in nitro:
-        code = ''.join([random.choice(string.ascii_letters + string.digits) for i in range(16)])
-    if prefix == "yes":
-        print(f"{Fore.WHITE}[ {Fore.GREEN}+ {Fore.WHITE}] {Fore.LIGHTBLACK_EX}Generated code {Fore.WHITE}{code}")
-        f.write(f"discord.gift/{code}\n")
-    else:
-        print(f"{Fore.WHITE}[ {Fore.GREEN}+ {Fore.WHITE}] {Fore.LIGHTBLACK_EX}Generated code {Fore.WHITE}{code}")
-        f.write(f"{code}\n")
+
+rproxy = p.read().split('\n')
+
+if checker != "yes":
+    while amount > 0:
+        amount = amount - 1
+        if "boost" in nitro:
+            code = ''.join([random.choice(string.ascii_letters + string.digits) for i in range(24)])
+        else:
+            code = ''.join([random.choice(string.ascii_letters + string.digits) for i in range(16)])
+        if prefix == "yes":
+            print(f"{Fore.WHITE}[ {Fore.GREEN}+ {Fore.WHITE}] {Fore.LIGHTBLACK_EX}Generated code {Fore.WHITE}{code}")
+            f.write(f"discord.gift/{code}\n")
+        else:
+            print(f"{Fore.WHITE}[ {Fore.GREEN}+ {Fore.WHITE}] {Fore.LIGHTBLACK_EX}Generated code {Fore.WHITE}{code}")
+            f.write(f"{code}\n")
+
+else:
+    while amount > 0:
+        f = open(f"working-codes.txt","a", encoding="UTF-8")
+        if not rproxy[0]:
+            print(f"{Fore.WHITE}[ {Fore.RED}! {Fore.WHITE}] {Fore.LIGHTBLACK_EX}All proxies are invalid!{Fore.WHITE}")
+            exit()
+        proxi = random.choice(rproxy)
+        proxies = {
+            "https": proxi
+        }
+        amount = amount - 1
+        if "boost" in nitro:
+            code = ''.join([random.choice(string.ascii_letters + string.digits) for i in range(24)])
+        else:
+            code = ''.join([random.choice(string.ascii_letters + string.digits) for i in range(16)])
+        try:
+            url = requests.get(f"https://discordapp.com/api/v6/entitlements/gift-codes/{code}", proxies=proxies, timeout=3)
+            if url.status_code == 200:
+                print(f"{Fore.WHITE}[ {Fore.GREEN}+ {Fore.WHITE}] {Fore.LIGHTBLACK_EX}Working Code {Fore.WHITE}{code}{Fore.WHITE}")
+                f.write(f"\ndiscord.gift/{code}")
+                f.close()
+            elif url.status_code == 404:
+                fulla = fulla - 1
+                print(f"{Fore.WHITE}[ {Fore.RED}- {Fore.WHITE}] {Fore.LIGHTBLACK_EX}Invalid Code {Fore.WHITE}{code}")
+            elif url.status_code == 429:
+                fulla = fulla - 1
+                print(f"{Fore.WHITE}[ {Fore.RED}- {Fore.WHITE}] {Fore.LIGHTBLACK_EX}Proxy {Fore.WHITE}{proxi}{Fore.LIGHTBLACK_EX} is ratelimited")
+                index = rproxy.index(proxi)
+                del rproxy[index]
+            else:
+                fulla = fulla - 1
+                print(f"{Fore.WHITE}[ {Fore.RED}! {Fore.WHITE}] {Fore.LIGHTBLACK_EX}Invalid Error! | Status code {Fore.WHITE}{url.status_code}")
+        except:
+            index = rproxy.index(proxi)
+            del rproxy[index]
+            pw = open(f"proxies.txt","w", encoding="UTF-8")
+            for i in rproxy:
+                pw.write(i + "\n")
+            pw.close()
+            fulla = fulla - 1
+            print(f"{Fore.WHITE}[ {Fore.RED}- {Fore.WHITE}] {Fore.LIGHTBLACK_EX}Failed connecting to proxy {Fore.WHITE}{proxi}{Fore.LIGHTBLACK_EX} | Removing from list!")
 
 print(f"{Fore.WHITE}[ {Fore.YELLOW}? {Fore.WHITE}] {Fore.LIGHTBLACK_EX}Succefully generated {Fore.WHITE}{fulla} {Fore.LIGHTBLACK_EX}codes!{Fore.WHITE}")
         
