5a95b6161df5f5c6bbc42c93a69588ed4b7d2f6e
lnxcz <david.frnoch@creativehill.cz>
2022-05-04 00:29:25 +0200

    add proxy scraper and code gen

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use rand::distributions::Alphanumeric;
+use rand::Rng;
+use std::thread;
+
+mod proxy_scrape;
+
 fn main() {
-    println!("Hello, world!");
+    let codes = generate_codes(10);
+    let proxies = proxy_scrape::scrape().unwrap();
+
+    println!("{:?}", proxies);
+    println!("{:?}", codes);
+
+    for i in 0..10 {
+        let code = codes[i].clone();
+
+        thread::spawn(move || {
+            println!("{:?}", code);
+        });
+    }
+}
+
+fn generate_codes(count: i32) -> Vec<String> {
+    let mut rng = rand::thread_rng();
+    let mut codes = Vec::new();
+
+    for _ in 0..count {
+        let code: String = (0..16).map(|_| rng.sample(Alphanumeric) as char).collect();
+
+        codes.push(code);
+    }
+    codes
 }
---
+++ src/proxy_scrape.rs
@@ -0,0 +1,29 @@
+use std::fs::File;
+use std::io::Write;
+
+pub fn scrape() -> Result<Vec<String>, reqwest::Error> {
+    let mut scraped = 0;
+    let mut f = File::create("proxies.txt").expect("Unable to create file");
+    f.write_all(b"").expect("Unable to write data");
+    let r = reqwest::blocking::get(
+        "https://api.proxyscrape.com/?request=displayproxies&proxytype=http&timeout=1500&ssl=yes",
+    )?
+    .text()?;
+    let mut proxies = vec![];
+
+    for proxy in r.split('\n') {
+        let proxy = proxy.trim();
+        if !proxy.is_empty() {
+            let proxy = format!("https://{}", proxy);
+            proxies.push(proxy);
+        }
+    }
+
+    for p in &proxies {
+        scraped = scraped + 1;
+        f.write_all(p.as_bytes()).expect("Unable to write data");
+        f.write_all(b"\n").expect("Unable to write data");
+    }
+    println!("[?] Scraped {} proxies.", scraped);
+    return Ok(proxies);
+}
